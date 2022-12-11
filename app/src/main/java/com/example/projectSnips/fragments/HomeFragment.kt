283050ad995c201f4f2c8324803a06174b07bea7
jacob <jacob.belizario11@gmail.com>
2022-12-11 17:43:57 -0500

    nav works with a bug

--- app/src/main/java/com/example/projectSnips/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/projectSnips/fragments/HomeFragment.kt
@@ -5,12 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.example.projectSnips.R
+import com.example.projectSnips.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
-//    private var _binding: FragmentHomeBinding? = null
-//    private val binding get() = _binding!!
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,24 +22,29 @@ class HomeFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        val navHostFragment =
-//            parentFragmentManager.findFragmentById(binding.fragmentBody.id) as NavHostFragment
-//        val navController = navHostFragment.navController
-//
-//        view?.findViewById<BottomNavigationView>(binding.bottomNavigationView.id)
-//            ?.setupWithNavController(navController)
+        val navHostFragment =
+            childFragmentManager.findFragmentById(R.id.fragment_body) as NavHostFragment
+        val navController = navHostFragment.navController
 
-        val navHostFragment = parentFragmentManager
+        setupWithNavController(binding.bottomNavigationView,navController)
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 
